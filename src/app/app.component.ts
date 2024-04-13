7f0741a7446fd4dbe86f72b1da6a62dd0e12fd3e
aniketchopade <aniketchopade@gmail.com>
2024-04-12 21:16:51 -0400

    add datatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatatableComponent } from './datatable/datatable.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatatableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'datatableapp';
